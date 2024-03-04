818f8e1f3d0d16fcc31224f5c4ce07556de0e146
Tristan Kitto <kittr801@student.otago.ac.nz>
2024-03-05 11:59:35 +1300

    Created method to check if there is one set of 3

--- Crit.java
+++ Crit.java
@@ -8,4 +8,14 @@ public class Crit extends Rollin {
         return 0;
     }
 
+    public static int[] oneSet(int[] dice) {
+        for (int[][] si : setIndices) {
+            if (isSet(si[0], dice)) {
+                return si[0];
+            } else if (isSet(si[1], dice)) {
+                return si[1];
+            }
+        }
+        return new int[] { -1, -1, -1 };
+    }
 }
